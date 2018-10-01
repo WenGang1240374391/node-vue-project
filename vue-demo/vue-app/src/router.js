9b293ee1a674d551a8b4cf5920bb4dd374e20c39
huas_wg <1240374391@qq.com>
2018-10-01 16:06:20 +0800

    just look

--- vue-demo/vue-app/src/router.js
+++ vue-demo/vue-app/src/router.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import index from "./components/index.vue"
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: []
+  routes: [{
+    path: "/",
+    component: index,
+    redirect: "/index"
+  }, {
+    path: "/index",
+    component: index,
+    name: "index"
+  }]
 })
